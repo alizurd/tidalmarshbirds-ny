d3c1c31a182aa477bfd90e326af4108c6e20c342
Alyssa Bueno <149504425+alizurd@users.noreply.github.com>
2025-02-01 19:44:29 -0500

    Update standardize_vegan.r
    more testing and viz

--- standardize_vegan.r
+++ standardize_vegan.r
@@ -6,7 +6,16 @@ setwd("~/Desktop")
 sharp <- read.csv("SHARP_surveyData_2014.csv", stringsAsFactors = FALSE)
 View(sharp)
 colnames(sharp) <- tolower(colnames(sharp))
-keep_columns <- c("state"
+
+str(sharp)
+
+sharp$tempf <- as.numeric(sharp$tempf)
+sharp$windsp <- as.numeric(sharp$windsp)
+sharp$sky <- as.numeric(sharp$sky)
+sharp$noise <- as.numeric(sharp$noise)
+sharp$surveydaye <- as.Date(sharp$surveydate)
+
+## keep_columns <- c("state"
                   ,"pointid" 
                   ,"point_x" 
                   ,"point_y" 
@@ -21,8 +30,27 @@ keep_columns <- c("state"
                   ,"alphacode"
                   ,"distance"
                   ,"totalcountn")
+
+keep_columns <- c("pointid"
+                  , "alphacode"
+                  , "totalcountn"
+                  , "windsp"
+                  , "noise"
+                  , "distance")
+
 ## convert the columns to numerics using as.numeric
 ## drop some of the columns as needed
-sharp <- aggregate(. ~ surveydate, data=sharp[,keep_columns], FUN = sum)
-decostand(sharp, method = "standardize", na.rm = FALSE)
+
+## checks
+nrow(sharp)
+unique(sharp$surveydate)
+unique(sharp$pointid)
+sharp <- aggregate(.~alphacode, data=sharp[,keep_columns], FUN = sum)
+
+##decostand(sharp, method = "standardize", na.rm = FALSE)
+
 View(sharp)
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(sharp, aes(x=alphacode)) +
+  geom_bar()
